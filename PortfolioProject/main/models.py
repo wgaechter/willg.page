6b95797f42aba79291a121d7e3cf4728eda6eb1f
will <will.gaechter@gmail.com>
2021-12-27 19:48:54 -0500

    Certification Model working and loading in HTML

--- PortfolioProject/main/models.py
+++ PortfolioProject/main/models.py
@@ -11,6 +11,17 @@ class Projects(models.Model):
     link = models.URLField()
     img = models.FilePathField(path="static/img")
 
+class Certifications(models.Model):
+    org = models.CharField(max_length=100) #IBM/Amazon Web Services
+    cert_name = models.CharField(max_length=100) #Data Science Professional Certification Badge/Cloud Practitioner Certification
+    synop = models.CharField(max_length=100) #Introduction to Data Science and its principles
+    cert_year = models.CharField(max_length=4) #2020
+    cert_month = models.CharField(max_length=10) #April
+    desc = models.TextField() #Any Additional Desc. necessary 
+    test_score = models.CharField(max_length=100) #if applicable (IF NOT: NA)
+
+
+
 
 
 
