c08091a01434d1c0724c3fa9a0a8afa82454dbec
wgaec <wgaec@WILL>
2024-11-06 09:50:49 -0500

    Add project files.

---
+++ PortfolioWebsite/Pages/Projects.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortfolioWebsite.Pages
+{
+    public class ProjectsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ PortfolioWebsite/Pages/Resume.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortfolioWebsite.Pages
+{
+    public class ResumeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
