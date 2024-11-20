f253e439ce4c02c92c4984133e3a6c28ed53229c
Will Gaechter <wgaechter@gmail.com>
2024-11-20 10:57:20 -0500

    Created model and base HTML

---
+++ PortfolioWebsite/Models/ContactFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace PortfolioWebsite.Models
+{
+    public class ContactFormModel
+    {
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public string Email { get; set; }
+        public string Body { get; set; }
+        public IFormFile Attachment { get; set; }
+    }
+}
